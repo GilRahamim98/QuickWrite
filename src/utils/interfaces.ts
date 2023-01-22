bb9d753de530666ca0cbf8a215f114b59d02385b
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-22 20:11:15 +0200

    Added option for header

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,11 @@
+export interface OptionInterface {
+    label: string,
+    result: boolean,
+    onChangeFunction: Function
+}
+
+export interface SuggstionInterface {
+    header: string,
+    article: string
+
+}
